13a0064e29c2269ff17f0a75a3cc6e884da57af7
Qasim <muhammadqasim.zip@gmail.com>
2026-04-07 04:35:40 +0500

    Email Fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import emailjs from '@emailjs/browser'
+
+// Initialize EmailJS
+emailjs.init('f9ErDzMnEKAjRsejA')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
